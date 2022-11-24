11c9bf6523b8011ac16af3440e6791589a9ba70b
juancansi <juancansi@gmail.comgit config --global user.name Your>
2022-11-24 00:39:33 -0300

    box shadow

--- appStyles.js
+++ appStyles.js
@@ -14,5 +14,13 @@ export const styles = StyleSheet.create({
     width: 300,
     height: 300,
     marginTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
   },
 });
