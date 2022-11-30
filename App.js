5787723d1eabf893a6a93b3d1ff1a77ccff52d4f
juancansi <juancansi@gmail.comgit config --global user.name Your>
2022-11-30 00:10:04 -0300

    delete unnecessary commits

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { styles } from './appStyles';
 import MapIframe from './src/components/MapIframe/MapIframe';
 
