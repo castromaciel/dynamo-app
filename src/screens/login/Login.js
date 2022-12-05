f1021a7890804d310cc0623db7c707121347eda4
agigli <giglimagustin@gmail.com>
2022-12-05 05:17:51 -0300

    Add Login UI

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,30 +1,87 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { FacebookSocialButton, GoogleSocialButton } from '../../components';
+import {
+  View, Image, Text, TextInput, Button, KeyboardAvoidingView, TouchableOpacity,
+} from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 import { styles } from './loginSytles';
 
 const Login = ({ navigation }) => {
+  const emailValidation = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    navigation.navigate('Login');
+  };
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior='padding'
+    >
        <Image
         style={styles.principalImage}
         source={require('../../../assets/img/logoRolling.png')}
       />
-      <View>
-        <FacebookSocialButton
-          buttonText={'Continuar con google'}
-          onPress={() => { navigation.navigate('Dashboard'); }}
+      <View style={styles.inputContainer}>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+            pattern: emailValidation,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="email"
         />
-        <GoogleSocialButton
-          buttonText='Continuar con Google'
-          onPress={() => { navigation.navigate('Dashboard'); }}
+        {errors.email && <Text>This is required.</Text>}
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry
+            />
+          )}
+          name="password"
         />
+        {errors.password && <Text>This is required.</Text>}
       </View>
-      <Text>
+      <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={handleSubmit(onSubmit)}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => { }}
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      <Text style={styles.bottomText}>
         Si continúas, aceptas los Términos del servicio de Dynamo y
         confirmas que has leído nuestra Política de privacidad.
       </Text>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 export default Login;
--- src/screens/login/loginSytles.js
+++ src/screens/login/loginSytles.js
@@ -3,8 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-evenly',
-    paddingHorizontal: 10,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   principalImage: {
@@ -17,5 +16,48 @@ export const styles = StyleSheet.create({
   socialIcon: {
     width: 13,
     height: 13,
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+    alignItems: 'center',
+  },
+  bottomText: {
+    marginTop: 100,
   },
 });
