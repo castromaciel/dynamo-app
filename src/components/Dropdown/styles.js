b71d4a592e18d6db666c2cdb7e9b4714492330d5
juancansi <juancansi@gmail.comgit config --global user.name Your>
2022-12-12 18:30:53 -0300

    chang Styles

--- src/components/Dropdown/styles.js
+++ src/components/Dropdown/styles.js
@@ -4,7 +4,6 @@ export const styles = StyleSheet.create({
   dropdownContainer: {
     position: 'absolute',
     width: 200,
-    height: 102,
     backgroundColor: '#fff',
     top: 43,
     right: 2,
